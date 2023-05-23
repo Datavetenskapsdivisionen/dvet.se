c72437defa068d33db6ba691b9cc7db8085b60fb
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-05-23 23:19:00 +0200

    Updated to `createRoot` instead of `ReactDOM`.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./app";
-ReactDOM.render(<App />, document.getElementById("app"));
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(<App />);
 
