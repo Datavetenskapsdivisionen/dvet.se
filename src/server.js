cd4c6252ddf28e73086a7ceb45a9b8f2936cf0d1
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-05-23 22:28:15 +0200

    Fixed some routing errors.

--- src/server.js
+++ src/server.js
@@ -2,10 +2,18 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+
+const callback = (req, res) => {
+    res.sendFile(path.join(__dirname, "../dist/index.html"));
+};
+
 app.use(express.static("dist"));
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist/index.html"));
-});
+app.get("/", callback);
+app.get("/committees", callback);
+app.get("/about", callback);
+app.get("/documents", callback);
+app.get("/contact", callback);
+
 
 const port = process.env.PORT || 8080;
 console.log("Server starting at: http://localhost:" + port);
