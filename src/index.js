28fdff44add5ac7348a0401929bc5a4c5742c08d
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-05-23 11:39:14 +0200

    Removed .hot

--- src/index.js
+++ src/index.js
@@ -3,6 +3,4 @@ import ReactDOM from "react-dom";
 
 import App from "./app";
 ReactDOM.render(<App />, document.getElementById("app"));
-// hot reloading. It works by replacing a module of the application
-// during runtime with an updated one so that it’s available for instant use.
-module.hot.accept();
+
