544a799567566b6f58b8c97e088bcb278ac37fc2
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-05-23 11:41:37 +0200

    Moved the homepage text over to the right page.

--- src/components/documents-page.jsx
+++ src/components/documents-page.jsx
@@ -1,11 +1,8 @@
 import React from "react";
-import text from "../../Content/Home.md";
-import ReactMarkdown from "react-markdown";
 
 const me = () => (
   <div>
     <h1>Documents</h1>
-    <ReactMarkdown children={text}></ReactMarkdown>
   </div>
 );
 
--- src/components/home-page.jsx
+++ src/components/home-page.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import ReactMarkdown from "react-markdown";
+import text from "../../Content/Home.md";
 
 const me = () => (
   <div>
     <h1>Home</h1>
+    <ReactMarkdown children={text}></ReactMarkdown>
   </div>
 );
 
