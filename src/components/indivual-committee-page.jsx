9c139a152faa7e074b571e92722f7c77a7b19f6b
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-05-30 14:15:38 +0200

    Added some boilerplate committee content.

--- src/components/indivual-committee-page.jsx
+++ src/components/indivual-committee-page.jsx
@@ -3,8 +3,13 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 
+rehypeRaw({ allowDangerousHtml: true });
+
 const me = (props) => {
-    return <ReactMarkdown children={props.text} rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}></ReactMarkdown>;
+    return <div className="page">
+        <ReactMarkdown children={props.text} rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}>
+        </ReactMarkdown>
+    </div>;
 };
 
 export default me;
