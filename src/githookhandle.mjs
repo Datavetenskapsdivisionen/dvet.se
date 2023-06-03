39d650d8a81f05ee2b6816266594525742bf5c43
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-06-03 13:22:44 +0200

    Fixed it again.

--- src/githookhandle.mjs
+++ src/githookhandle.mjs
@@ -22,7 +22,7 @@ const handleHook = async (hookData) => {
             const body = issue.body;
 
             const content = {
-                content: "# [" + title + "](url)\n" + body,
+                content: "# " + title + "\n" + body + "\n\nLink: <" + url + ">",
                 username: name,
                 avatar_url: avatar,
                 embeds: [],
