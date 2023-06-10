6b5ce971733ebe4806b09d5a2aba9a14bde94ef8
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-06-10 16:18:31 +0200

    Fixed a typo.

--- src/www/components/widgets/newsfeed.jsx
+++ src/www/components/widgets/newsfeed.jsx
@@ -4,7 +4,7 @@ import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 
 const fetchNews = () => (
-    fetch("http://localhost:8080/newsfeed")
+    fetch("/newsfeed")
         .then(res => res.json())
         .catch(e => ({
             error: "failed to fetch news"
