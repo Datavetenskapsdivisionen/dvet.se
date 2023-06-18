c25b81f5afb0b395a964eec8b4cc77bc60c36ff4
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-06-18 21:03:25 +0200

    Updated some dates.

--- src/www/util.js
+++ src/www/util.js
@@ -1,6 +1,6 @@
 const isReception = () => {
-    const fromDate = new Date(Date.parse(`${new Date().getFullYear()}-07-13`));
-    const toDate = new Date(Date.parse(`${new Date().getFullYear()}-09-16`));
+    const fromDate = new Date(Date.parse(`${new Date().getFullYear()}-07-12`));
+    const toDate = new Date(Date.parse(`${new Date().getFullYear()}-09-17`));
     return (new Date().getDate() >= fromDate.getDate())
         && (new Date().getDate() <= toDate.getDate());
 };
