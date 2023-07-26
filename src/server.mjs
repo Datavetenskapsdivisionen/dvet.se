2ebc3d0545c4d4cc653eea98302d341f5c266369
timdekstop <timheterjag@gmail.com>
2023-07-26 19:48:25 +0200

    did a fuckywucky

--- src/server.mjs
+++ src/server.mjs
@@ -25,7 +25,7 @@ app.use(expressStaticGzip("dist", {
 app.use(express.json());
 app.get("/", callback);
 app.get("/committees", callback);
-app.get("/newscreen", callback);
+app.get("/newsscreen", callback);
 app.get("/about", callback);
 app.get("/documents", callback);
 app.get("/contact", callback);
