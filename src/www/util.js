e00f7be0735aee61ee76424037be0547cee68842
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2023-07-27 23:37:16 +0200

    Fixed the event button not appearing.

--- src/www/util.js
+++ src/www/util.js
@@ -1,8 +1,10 @@
 const isReception = () => {
     const fromDate = new Date(Date.parse(`${new Date().getFullYear()}-07-12`));
     const toDate = new Date(Date.parse(`${new Date().getFullYear()}-09-17`));
-    return (new Date().getDate() >= fromDate.getDate())
-        && (new Date().getDate() <= toDate.getDate());
+    const res = (new Date().getTime() >= fromDate.getTime())
+        && (new Date().getTime() <= toDate.getTime());
+    console.log(res);
+    return res;
 };
 
 const getLanguageCookie = () => {
