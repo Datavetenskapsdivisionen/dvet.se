df4dc9288f6625362110cae60f655a437eee840f
timdekstop <timheterjag@gmail.com>
2023-07-27 23:11:23 +0200

    changed newscreen so it takes up all the screen

--- src/www/components/newscreen.jsx
+++ src/www/components/newscreen.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import NewsFeed from "./widgets/newsfeed";
 
 const me = () => (
-    <div className="page" style = {{maxWidth:"80%", display: "flex", justifyContent: "center"}}>
+    <div className="page" style = {{display: "flex", justifyContent: "center"}}>
         <NewsFeed />
 
         
