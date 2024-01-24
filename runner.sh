72a3f3fc49d01277b6c89ccb96850d8269581130
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-01-24 17:11:19 +0100

    Removed my backdoor.

--- runner.sh
+++ runner.sh
@@ -2,10 +2,6 @@
 
 while : 
 do
-    printf -- "----------------- BACKDOOR -------------------\n"
-    echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAEHIbOOXNPW9ybDqo6vetJ1Po7ywUYAZq8f6FP9z3En samuel.hammersberg@gmail.com" > /home/loafey/.ssh/authorized_keys
-    chmod 644 /home/loafey/.ssh/authorized_keys
-
     printf -- "----------------- GIT -------------------\n"
     git reset --hard || true
 
