fde9527909902e9bb4789aad7121c6357f983570
lukas-gartman <gusgartlu@student.gu.se>
2024-04-05 16:45:44 +0200

    Fix img preview, show children count & hide empty dirs

--- src/www/components/photos-page.jsx
+++ src/www/components/photos-page.jsx
@@ -35,30 +35,40 @@ const createElements = (data, searchParams, setSearchParams) => {
 
     const children = data.children ? data.children.map(c => {
         if (c.mimeType == "application/vnd.google-apps.folder") {
-            return <div
-                className="item"
-                onClick={() => {
-                    path.push(c.id);
-                    updatePath();
-                }}>
-                <svg xmlns="http://www.w3.org/2000/svg" height="99" viewBox="0 0 24 24" width="160">
-                    <path d="M0 0h24v24H0z" fill="none" />
-                    <path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z" />
-                </svg>
-                <span>{c.name}</span>
-            </div>;
+            if (c.childrenCount > 0) {
+                return <div
+                    className="item"
+                    onClick={() => {
+                        path.push(c.id);
+                        updatePath();
+                    }}>
+                    <span className="dir-count">{c.childrenCount}</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="99" viewBox="0 0 24 24" width="160">
+                        <path d="M0 0h24v24H0z" fill="none" />
+                        <path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z" />
+                    </svg>
+                    <span className="title-bar">{c.name}</span>
+                </div>;
+            }
         } else {
             return <div
-                className="item"
+                className="item preview-holder"
                 onClick={() => window.open(c.url)}
             >
-                <div className="preview-holder">
-                    <div className="overlay"></div>
-                    <iframe className="preview" src={c.previewUrl} allow="autoplay"
-                        width="160px"
-                        height="90px" />
+                <div className="overlay">
+                    { c.mimeType.includes("video") ? 
+                        <svg viewBox="0 0 200 200" width="60px" height="60px" alt="Play video">
+                            <circle cx="100" cy="100" r="90" fill="none" stroke-width="12" stroke="#fff" stroke-opacity="0.8" />
+                            <circle cx="100" cy="100" r="97" fill="none" stroke="black" stroke-width="1px" />
+                            <circle cx="100" cy="100" r="83" fill="none" stroke="black" stroke-width="1px" />
+                            <polygon points="70, 55 70, 145 145, 100" fill="none" stroke="#fff" stroke-opacity="0.8" stroke-width="8" />
+                            <polygon points="65, 46 65, 154 155, 100" fill="none" stroke="black" stroke-width="1px" />
+                            <polygon points="75, 64 75, 136 135, 100" fill="none" stroke="black" stroke-width="1px" />
+                    </svg> 
+                    : <></> }
                 </div>
-                <span>{c.name}</span>
+                <img className="preview" src={c.thumbnailUrl} referrerPolicy="no-referrer" />
+                {/* <span className="title-bar">{c.name}</span> */}
             </div>;
         }
     }) : [];
@@ -75,7 +85,7 @@ const fetchImageData = () => (
     fetch("/getPhotos")
         .then(res => res.json())
         .catch(e => ({
-            error: "failed to fetch images"
+            error: isEnglish() ? "failed to fetch images" : "kunde inte hämta bilder"
         })));
 
 const me = () => {
@@ -91,11 +101,11 @@ const me = () => {
         {data}
         {isEnglish() ? <div>
             <br />
-            If you find any photos you wish to be removed here please contact <a href="styrelsen@dvet.se">styrelsen@dvet.se </a>
+            If you find any photos you wish to be removed here please contact <a href="mailto:styrelsen@dvet.se">styrelsen@dvet.se </a>
             and we will remove them!
         </div> : <div>
             <br />
-            Om du hittar några bilder som du önskar ska bli borttagna så kan du kontakta <a href="styrelsen@dvet.se">styrelsen@dvet.se </a>
+            Om du hittar några bilder som du önskar ska bli borttagna så kan du kontakta <a href="mailto:styrelsen@dvet.se">styrelsen@dvet.se </a>
             så tar vi bort dom!
         </div>}
     </div>;
