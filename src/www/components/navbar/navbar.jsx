d1e6f6a72f2c808cd5c39174522f4028a837bc9c
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-06-05 00:11:36 +0200

    Replace About Us with the wiki.

--- src/www/components/navbar/navbar.jsx
+++ src/www/components/navbar/navbar.jsx
@@ -8,11 +8,10 @@ const navbar = () => {
       <div>
         <NavLink className="nav__link" to="/">Start</NavLink>
         <NavLink className="nav__link" to="/committees">{isEnglish() ? "Committees" : "Kommitteer"}</NavLink >
-        <NavLink className="nav__link" to="/about">{isEnglish() ? "About us" : "Om Oss"}</NavLink >
+        <NavLink className="nav__link" to="/dviki">{"Wiki"}</NavLink >
         <NavLink className="nav__link" to="/photos">{isEnglish() ? "Photos" : "Bilder"}</NavLink >
         <NavLink className="nav__link" to="/documents">{isEnglish() ? "Documents" : "Dokument"}</NavLink >
         <NavLink className="nav__link" to="/contact">{isEnglish() ? "Contact" : "Kontakt"}</NavLink >
-        <NavLink className="nav__link" to="/dviki">{"Wiki"}</NavLink >
       </div>
     </nav>
   );
