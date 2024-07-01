c7e34c9b11ec084b78171d4b7d0caef8d4a8271c
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-07-01 19:33:10 +0200

    Add logout button.

--- src/www/components/navbar/footer.jsx
+++ src/www/components/navbar/footer.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { isAuth } from "../../util";
+import Cookies from "js-cookie";
 
 const footer = () => {
+    let [loggedIn, setLoggedIn] = useState(false);
+    useEffect(() => {
+        const eff = async () => setLoggedIn(await isAuth());
+        eff();
+    }, []);
+
+    const logOutButton = loggedIn
+        ? <button onClick={() => {
+            Cookies.remove("dv-token");
+            location.reload();
+        }}>Logout</button>
+        : <></>;
+
     return (
         <footer>
-            <div></div>
+            <span />
             <span>© dvet.se {new Date().getFullYear()}</span>
-            <button onClick={() => {
-                document.cookie = "language=null; path=/";
-                location.reload();
-            }}>Switch Language</button>
-            {/* <div id="google_translate_element"></div> */}
+            <div className="button-container">
+                {logOutButton}
+                <button onClick={() => {
+                    Cookies.set("language", "null");
+                    location.reload();
+                }}>Language</button>
+            </div>
         </footer>
     );
 };
