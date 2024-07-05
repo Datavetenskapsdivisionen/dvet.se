7a30e892332ba2fa6c0d60197dd07126f43803c6
tim@dvet.se <timheterjag@gmail.com>
2024-07-05 19:21:10 +0200

    Updte

--- webpack.config.js
+++ webpack.config.js
@@ -83,7 +83,7 @@ module.exports = {
         use: ["style-loader", "css-loader"],
       },
       {
-        test: /\.png|jpg|gif$/,
+        test: /\.png|jpg|gif|pdf$/,
         use: ["file-loader"],
       },
       {
