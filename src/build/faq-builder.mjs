0ad698f1fac642717f9ec4d52cf1f70c12f1bdb5
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2024-07-19 23:36:33 +0200

    Fix layout when opening questions.

--- src/build/faq-builder.mjs
+++ src/build/faq-builder.mjs
@@ -7,36 +7,70 @@ let outputEn = "";
 if (fs.existsSync("faq-cache")) fs.rmSync("faq-cache", { recursive: true, force: true });
 fs.mkdirSync("faq-cache");
 
+const createQuestion = question => {
+    let output = "";
+    let outputEn = "";
+
+    let content = question.content;
+    let contentEn = question.contentEn;
+
+    output += `<div class="faq-question">
+        <div class="faq-question-title">
+            <button class="faq-expand-button">
+                <h2>${question.name ?? "Inget Namn"}</h2>
+                <a>▼</a>
+            </button>
+        </div>
+    <div class="faq-container" style="height: 0;"><div class="faq-content">`;
+    outputEn += `<div class="faq-question">
+        <div class="faq-question-title">
+            <button class="faq-expand-button">
+                <h2>${question.name ?? "No Name"}</h2>
+                <a>▼</a>
+            </button>
+        </div>
+    <div class="faq-container" style="height: 0;"><div class="faq-content">`;
+    output += content ?? "<p>Ingen Text</p>";
+    outputEn += contentEn ?? "<p>No Text</p>";
+    output += `</div></div></div>`;
+    outputEn += `</div></div></div>`;
+
+    return [output, outputEn];
+};
+
 for (const category of Faq) {
     output += `<div><h2>${category.name ?? "Inget Namn"}</h2>`;
     outputEn += `<div><h2>${category.name_en ?? "No Name"}</h2>`;
     output += `<div class="faq-category">`;
     outputEn += `<div class="faq-category">`;
-    for (const question of category.questions) {
-        let content = question.content;
-        let contentEn = question.contentEn;
 
-        output += `<div class="faq-question">
-            <div class="faq-question-title">
-                <button class="faq-expand-button">
-                    <h2>${question.name ?? "Inget Namn"}</h2>
-                    <a>▼</a>
-                </button>
-            </div>
-        <div class="faq-container" style="height: 0;"><div class="faq-content">`;
-        outputEn += `<div class="faq-question">
-            <div class="faq-question-title">
-                <button class="faq-expand-button">
-                    <h2>${question.name ?? "No Name"}</h2>
-                    <a>▼</a>
-                </button>
-            </div>
-        <div class="faq-container" style="height: 0;"><div class="faq-content">`;
-        output += content ?? "<p>Ingen Text</p>";
-        outputEn += contentEn ?? "<p>No Text</p>";
-        output += `</div></div></div>`;
-        outputEn += `</div></div></div>`;
+    let q1 = [];
+    let q2 = [];
+    let flippy = true;
+    for (const question of category.questions) {
+        if (flippy) q1.push(question);
+        else q2.push(question);
+        flippy = !flippy;
     }
+
+    output += `<div class="faq-category-split">`;
+    outputEn += `<div class="faq-category-split">`;
+    q1.forEach(question => {
+        let [o, oEn] = createQuestion(question);
+        output += o;
+        outputEn += oEn;
+    });
+    output += "</div>";
+    outputEn += "</div>";
+    output += `<div class="faq-category-split">`;
+    outputEn += `<div class="faq-category-split">`;
+    q2.forEach(question => {
+        let [o, oEn] = createQuestion(question);
+        output += o;
+        outputEn += oEn;
+    });
+    output += "</div>";
+    outputEn += "</div>";
 
     output += `</div></div>`;
     outputEn += `</div></div>`;
