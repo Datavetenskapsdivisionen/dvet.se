af2849e5c36bab0c8cc7bb4f6924e81bdc9c1ba6
lukas-gartman <gusgartlu@student.gu.se>
2024-11-20 00:02:55 +0100

    Fix GitHub auth bug

--- src/route-handlers/github-auth.mjs
+++ src/route-handlers/github-auth.mjs
@@ -21,6 +21,8 @@ const githubLogin = async (req, res) => {
 const githubCallback = async (req, res, next) => {
     const isLocalhost = process.env.GITHUB_DEV_APP_SECRET !== undefined;
     const code = req.query.code;
+    if (!code) { return res.status(405).redirect("/"); }
+    
     const tokenResponse = await fetch('https://github.com/login/oauth/access_token', {
         method: 'POST',
         headers: {
@@ -35,21 +37,26 @@ const githubCallback = async (req, res, next) => {
         }),
     });
 
-    if (!tokenResponse.ok) {
+    const tokenResponseJson = await tokenResponse.json();
+
+    if (!tokenResponseJson.ok || !tokenResponseJson.access_token) {
         console.log(tokenResponse);
         return res.status(401).send("Unable to authenticate with GitHub.");
     }
 
-    const tokenResponseJson = await tokenResponse.json();
-    const accessToken = tokenResponseJson.access_token;
-    
-    const octokit = new Octokit({ auth: accessToken });
-    const { data: user } = await octokit.rest.users.getAuthenticated();
+    try {
+        const accessToken = tokenResponseJson.access_token;
+        const octokit = new Octokit({ auth: accessToken });
+        const { data: user } = await octokit.rest.users.getAuthenticated();
 
-    const userJwt = jwt.sign(user, process.env.JWT_SECRET_KEY);
-    res.cookie("dv-github-user", userJwt);
-    res.cookie("dv-github-token", accessToken, { httpOnly: true, secure: true });
-    next();
+        const userJwt = jwt.sign(user, process.env.JWT_SECRET_KEY);
+        res.cookie("dv-github-user", userJwt);
+        res.cookie("dv-github-token", accessToken, { httpOnly: true, secure: true });
+        next();
+    } catch (e) {
+        console.log(e);
+        res.status(401).send("Unable to authenticate with GitHub");
+    }
 };
 
 export { isAuthWithGithub, githubLogin, githubCallback };
