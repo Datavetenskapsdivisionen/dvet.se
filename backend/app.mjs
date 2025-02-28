9fb80566ecdb25edd79d977e1b1e3437f3ac5648
lukas-gartman <gusgartlu@student.gu.se>
2025-03-01 00:35:22 +0100

    Move pdf and redirect routes to separate files

--- backend/app.mjs
+++ backend/app.mjs
@@ -1,7 +1,6 @@
 import "dotenv/config";
 import express from "express";
 import cookieParser from "cookie-parser";
-import path from "path";
 import expressStaticGzip from "express-static-gzip";
 import chalk from "chalk";
 
@@ -10,7 +9,10 @@ import { postHook } from "./route-handlers/githookhandle.mjs";
 
 import apiRoutes from "./routes/api-routes.mjs";
 import frontendRoutes from "./routes/frontend-routes.mjs";
+import pdfRoutes from "./routes/pdf-routes.mjs";
+import redirectRoutes from "./routes/redirect-routes.mjs";
 
+// Check for missing environment variables
 if (!process.env.KILL_TOKEN)          console.warn(chalk.yellow("[WARNING] KILL_TOKEN was not found in .env - You won't be able to restart the server remotely."));
 if (!process.env.ENABLE_DRIVE)        console.warn(chalk.yellow("[WARNING] ENABLE_DRIVE is false or was not found in .env - Google Drive features will be disabled."));
 if (!process.env.WEBHOOK_SECRET)      console.warn(chalk.yellow("[WARNING] WEBHOOK_SECRET was not found in .env - GitHub webhook features will be disabled."));
@@ -20,11 +22,7 @@ if (!process.env.GITHUB_APP_CLIENT_ID ||
     !process.env.GITHUB_APP_SECRET)   console.warn(chalk.yellow("[WARNING] GITHUB_APP_CLIENT_ID and GITHUB_APP_SECRET were not found in .env - Certain GitHub features that requires logging in will be disabled."));
 if (!process.env.JWT_SECRET_KEY)      console.warn(chalk.yellow("[WARNING] JWT_SECRET_KEY was not found in .env - You won't be able to store user information."));
 
-const servePdf = (req, res, pdf) => {
-    const filePath = path.join(process.cwd(), pdf);
-    res.sendFile(filePath);
-};
-
+// Create express app
 const app = express();
 
 // Middleware for handling trailing slashes and non-www requests
@@ -47,25 +45,24 @@ app.get("/dist-secret/:path*", (req, res) => {
     res.send(`"💩";`);
 });
 
+// Middlewares
 app.use(cookieParser());
 app.use(expressStaticGzip("frontend/dist", { // Middleware for serving static files (React frontend)
     serveStatic: { maxAge: 60 * 1000 }
 }));
 app.use(express.json()); // Middleware for parsing JSON
 
+// Routes
 app.use(apiRoutes);
 app.use(frontendRoutes);
+app.use(pdfRoutes);
+app.use(redirectRoutes);
 
+// Used externally
 app.post("/postHook", postHook);
 app.post("/killerBean", killerBean);
 
-app.get("/discord", (req, res) => res.redirect("https://discord.gg/BVyhSv4rVw"));
-app.get("/committees/board-of-studies", (req, res) => res.redirect("/committees/student-educational-committee"));
-app.get("/recceform", (req, res) => res.status(301).redirect("https://dvet.se/committees/dvrk/form"));
-
-app.get("/recceguiden", (req, res) => servePdf(req, res, "../frontend/assets/kick-off/recceguiden.pdf"));
-app.get("/masterguide", (req, res) => servePdf(req, res, "../frontend/assets/kick-off/masterguiden.pdf"));
-
+// Health check
 app.get("/ping", (req, res) => res.status(200).send("pong"));
 
 const port = process.env.PORT || 8080;
