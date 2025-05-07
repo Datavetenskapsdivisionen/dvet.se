566e84b2f610156d2fa9289bef382caa5b37b22f
lukas-gartman <gusgartlu@student.gu.se>
2025-05-07 20:41:23 +0200

    Return middleware upon redirect

--- backend/app.mjs
+++ backend/app.mjs
@@ -28,13 +28,13 @@ const app = express();
 // Middleware for handling trailing slashes and non-www requests
 app.use((req, res, next) => {
     if (req.hostname === "dvet.se") {
-        res.redirect(301, req.protocol + "://www.dvet.se" + req.originalUrl);
+        return res.redirect(301, req.protocol + "://www.dvet.se" + req.originalUrl);
     }
 
     if (req.path.slice(-1) === '/' && req.path.length > 1) {
         const query = req.url.slice(req.path.length);
         const safepath = req.path.slice(0, -1).replace(/\/+/g, '/');
-        res.redirect(301, safepath + query);
+        return res.redirect(301, safepath + query);
     } else {
        next();
     }
