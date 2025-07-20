dce9eb4aa7adb0d0a56547b37713e98bc4d9ed2d
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-07-20 13:00:50 +0200

    Fix faq buttons.

--- frontend/components/pages/dvrk-page.jsx
+++ frontend/components/pages/dvrk-page.jsx
@@ -146,6 +146,25 @@ const ContentHolder = (props) => (
 );
 
 const MainPage = () => {
+    useEffect(() => {
+        const buttons = document.getElementsByClassName("faq-expand-button");
+        for (let i = 0; i < buttons.length; i++) {
+            const button = buttons.item(i);
+            const arrow = button.children.item(1);
+            // :)
+            const content = button.parentElement.nextElementSibling;
+            const max = content.scrollHeight + "px";
+            button.addEventListener('click', () => {
+                if (content.style.height == max) {
+                    content.style.height = "0";
+                    arrow.innerHTML = "▼";
+                } else {
+                    content.style.height = max;
+                    arrow.innerHTML = "▲";
+                }
+            });
+        }
+    }, []);
     return (<ContentHolder element={<>
         <ReactMarkdown children={isEnglish() ? textEn : text} rehypePlugins={[rehypeRaw]} remarkPlugins={[remarkGfm]}></ReactMarkdown>
         <h2>FAQ</h2>
