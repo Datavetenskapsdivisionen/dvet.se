263430c8e516df795a62c6d6cb0aef44f3c911d9
Samuel Hammersberg <samuel.hammersberg@gmail.com>
2025-08-16 15:59:01 +0200

    Add Discord logging

--- runner.sh
+++ runner.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Set up logging
+if [ -f  ".log_uri" ]; then 
+    uri="$(cat ".log_uri")"
+    function send() {
+        data=$(echo "{\"content\": \"\`$(date): $1\`\"}"| sed -e 's/\x1b\[[0-9;]*m//g')
+        curl -sS \
+            -H "Accept: application/json" \
+            -H "Content-Type:application/json" \
+            -X POST --data "$data" "$uri" 2> /dev/null
+    } 
+else 
+    function send() {
+        echo "" > /dev/null
+    }
+    echo "Discord logging disabled"
+fi
+
+send "Starting from cold boot"
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
@@ -21,5 +40,9 @@ do
     npm run build || true
 
     printf -- "\n----------------- SER -------------------\n"
-    npm start
+    send "I be running 🏃‍♀️🏃‍♀️"
+    npm start 2> >(while IFS= read -r p; do
+        echo "$p"
+        send "$p"
+    done)
 done
